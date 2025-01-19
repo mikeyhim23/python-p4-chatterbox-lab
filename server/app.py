3b8953cb25a537b9554f1add3a37fe39ace9329f
Christopher Karanja Nganga <christopher.nganga@student.moringaschool.com>
2025-01-19 14:15:55 +0300

    fullstack app

--- server/app.py
+++ server/app.py
@@ -14,13 +14,56 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/messages')
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods=['POST'])
+def create_messages():
+    data = request.get_json()
+
+    body = data.get('body')
+    username = data.get('username')
+    
+    if not body or not username:
+        return jsonify({'error': 'Both "body" and "username" are required'}), 400
+    
+    new_message = Message(body=body, username=username)
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get(id)
+
+    if message is None:
+        return jsonify({'error': 'Message not found'}), 404
+    
+    data = request.get_json()
+    new_body = data.get('body')
+    
+    if not new_body:
+        return jsonify({'error': '"body" is required to update the message'}), 400
+    message.body = new_body
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    
+    db.session.delete(message)
+    db.session.commit()
+
+    return jsonify({'message': 'Message deleted successfully'}), 200
+
 
 if __name__ == '__main__':
     app.run(port=5555)
